4dcf32e70235159dab64e4093de69a815865f471
BelousovAD <ya.belousow-lesha2015@yandex.ru>
2024-04-29 20:11:51 +0700

    Создан скрипт сохранения инфентаря в JSON-файл

---
+++ Assets/GameResources/Features/Model/Scripts/SaverToJSON.cs
@@ -0,0 +1,48 @@
+namespace GameInventory.Feature.Model
+{
+    using System.IO;
+    using UnityEngine;
+
+    /// <summary>
+    /// Скрипт сохранения/загрузки объектов в/из JSON-файл.
+    /// </summary>
+    public class SaverToJSON
+    {
+        private string _pathToSave = string.Empty;
+
+        /// <summary>
+        /// Создаёт объект скрипта сохранения/загрузки.
+        /// </summary>
+        public SaverToJSON() { }
+
+        /// <summary>
+        /// Устанавливает путь для сохранения файлов.
+        /// </summary>
+        /// <param name="pathToSave">Путь для сохранения.</param>
+        public void Initialize(string pathToSave) => _pathToSave = pathToSave;
+
+        /// <summary>
+        /// Сохраняет объект в JSON-файл.
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <param name="fileName">Имя файла.</param>
+        public void Save(Object obj, string fileName)
+        {
+            string jsonString = JsonUtility.ToJson(obj);
+            string filePath = Path.Combine(_pathToSave, fileName + ".json");
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        /// <summary>
+        /// Загружает объект из JSON-файла.
+        /// </summary>
+        /// <param name="obj">Объект.</param>
+        /// <param name="fileName">Имя файла.</param>
+        public void Load(out Object obj, string fileName)
+        {
+            string filePath = Path.Combine(_pathToSave, fileName + ".json");
+            string jsonString = File.ReadAllText(filePath);
+            obj = JsonUtility.FromJson<Object>(jsonString);
+        }
+    }
+}
